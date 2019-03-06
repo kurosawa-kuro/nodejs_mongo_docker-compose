b63fcc3d7795304c722b62ec8f0a73223a8cfc52
Toshifumi Kurosawa <kuromailserver@gmail.com>
2019-03-06 14:38:40 +0900

    Docker化

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# nodejs_mongo_docker-compose
+# mean_mytasklist
+MEAN application to manage tasks
+This app is testing the Docker, GIT, Jenkins and AWS deployment automation.
+Part of the ESTATEABLY BLOCKCHAIN project.
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require("path");
 var bodyParser = require("body-parser");
 
 var index = require("./routes/index");
-// var tasks = require("./routes/tasks");
+var tasks = require("./routes/tasks");
 
 var port = 3000;
 
@@ -14,12 +14,15 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
+// Set Static Folder
+// app.use(express.static(path.join(__dirname, 'client')));
+
 // Body Parser MW
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", index);
-// app.use("/api", tasks);
+app.use("/api", tasks);
 
 app.listen(port, function() {
   console.log("Server started on port " + port);
